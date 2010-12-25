a87d42288d63c16ed58f7d8426777becf0263e6c
Rodrigo Navarro <rnavarro1@gmail.com>
2010-12-25 19:01:48 -0200

    Improving pooling function

--- public/javascripts/pool.js
+++ public/javascripts/pool.js
@@ -1,8 +1,9 @@
 jQuery(function($){
-  function longPool(response) {
-    var room = $('#room');
+  var maximumRetries = 3,
+      retries = 0;
 
-    appendMessage(response);
+  function longPool() {
+    var room = $('#room');
 
     $.ajax({ 
       cache: false,
@@ -11,14 +12,24 @@ jQuery(function($){
       dataType : 'html',
       data: { since: $('.message:last', room).data('time') || 1 },
       error: function() {
-        setTimeout(longPool, 10 * 1000);
+        if(++retries >= maximumRetries) {
+          // If we are getting repeating errors, we kick the user out of the room
+          window.location = '/';
+        } else {
+          // Ok, let's wait 10 seconds and try again
+          setTimeout(longPool, 10 * 1000);
+        }
       },
       success: function(data) {
+        appendMessage(data);
+
+        // Start pooling again
         longPool(data);
       }
     });
   }
 
+  // We start pooling as soon as the user enters the room
   longPool();
 
   $('form').submit(function(e){
